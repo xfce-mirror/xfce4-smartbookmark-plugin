4fdbb9fa5397d4e6caaf8565441fbc967d69929f
Andre Miranda <andreldm@xfce.org>
2019-10-19 19:46:16 -0300

    s/http/https

--- src/smartbookmark.c
+++ src/smartbookmark.c
@@ -173,7 +173,7 @@ static t_search *search_new(XfcePanelPlugin *plugin)
     gtk_widget_set_valign(GTK_WIDGET(search->box), GTK_ALIGN_CENTER);
 
     /* default options */
-    search->url = "http://bugs.debian.org/";
+    search->url = "https://bugs.debian.org/";
     search->label_text = "BTS";
     search->size = 5;
     search->hide_label = FALSE;
@@ -209,7 +209,7 @@ static void search_read_config(t_search *search, const gchar* filename)
     if( (rcfile = xfce_rc_simple_open(filename, TRUE) ))
     {
         xfce_rc_set_group(rcfile, NULL);
-        search->url = g_strdup(xfce_rc_read_entry(rcfile,"url","http://bugs.debian.org/"));
+        search->url = g_strdup(xfce_rc_read_entry(rcfile,"url","https://bugs.debian.org/"));
         search->label_text = g_strdup(xfce_rc_read_entry(rcfile,"value","DBS"));
         search->size = xfce_rc_read_int_entry(rcfile, "size", 5);
         search->hide_label = xfce_rc_read_bool_entry(rcfile, "hidelabel", FALSE);

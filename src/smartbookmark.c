f7449946000fe53158c3acb51d9b61fd4a22625c
Bobby Rong <rjl931189261@126.com>
2025-03-20 23:14:49 +0800

    Add meson build

--- src/smartbookmark.c
+++ src/smartbookmark.c
@@ -34,6 +34,9 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+#ifdef HAVE_XFCE_REVISION_H
+#include "xfce-revision.h"
+#endif
 
 #include <gtk/gtk.h>
 #include <gdk/gdkkeysyms.h>
@@ -256,7 +259,7 @@ static void show_about(XfcePanelPlugin *plugin, t_search *search)
     gtk_show_about_dialog(NULL,
         "logo-icon-name", "system-search",
         "license", xfce_get_license_text (XFCE_LICENSE_TEXT_GPL),
-        "version", PACKAGE_VERSION,
+        "version", VERSION_FULL,
         "program-name", PACKAGE_NAME,
         "comments", _("Query websites from the Xfce panel"),
         "website", PACKAGE_URL,
